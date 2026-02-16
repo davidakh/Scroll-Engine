4a16a67c97b4bfc603991fc65401617f2a41d619
David Akhmedbayev <david.akhmedbayev@icloud.com>
2026-02-16 13:43:17 -0500

    Fixed issue.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ScrollEngine",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v26),
         .macOS(.v13),
     ],
     products: [
